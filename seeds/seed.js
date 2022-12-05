9af779abd602f5526c30abc81e99a290ebbc5634
Baptiste Dromer <baptiste.dromer@gmail.com>
2022-12-05 15:37:42 +0100

    seed progress

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,23 +2,74 @@ require('dotenv').config();
 require('../config/dbConfig');
 
 const mongoose = require('mongoose');
+
+// models
 const User = require('../models/User.model');
+const Reservation = require('../models/Reservation.model');
+const Service = require('../models/Service.model');
+const ServiceItem = require('../models/ServiceItem.model');
+const Tag = require('../models/Tag.model');
+
+// seeds
+const tagSeed = require('./tag.seeds.json');
 const userSeed = require('./user.seeds.json');
+const servicesSeed = require('./services.seed.json');
 
-// const Tag = require('../models/Tag.model');
-// const tagSeed = require('./tag.seeds.json');
+const seedingtags = async () => {
+  try {
+    const deletedTags = await Tag.deleteMany();
+    console.log(`Deleted tags: ${deletedTags.deletedCount}`);
 
-// const Reservation = require('../models/Reservation.model');
-// const Service = require('../models/Service.model');
-// const ServiceItem = require('../models/ServiceItem.model');
+    const createdTags = await Tag.create(tagSeed);
+    console.log(`Created tags: ${createdTags.length}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const seeding = async () => {
+const seedingUsers = async () => {
   try {
     const deletedUsers = await User.deleteMany();
     console.log(`Deleted users: ${deletedUsers.deletedCount}`);
 
     const createdUsers = await User.create(userSeed);
     console.log(`Created users: ${createdUsers.length}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const seedingServicesConversations = async () => {
+  try {
+    deletedService = await Service.deleteMany();
+    for (let service of servicesSeed) {
+      const { _id: providerId } = (await User.find({ username: service.provider }, { _id: 1 }))[0];
+      const consumerId = await User.find({ username: service.consumer })[0];
+
+      const tags = await Tag.find({ $in: service.tags });
+
+      const createdService = await Service.create({
+        title: service.title,
+        description: service.description,
+        tags,
+        location: service.location,
+        provider: providerId,
+      });
+      console.log(createdService);
+      for (let { startDate, endDate, price } of service.serviceItems) {
+        await ServiceItem.create({ startDate, endDate, price });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const seeding = async () => {
+  try {
+    await seedingUsers();
+    await seedingtags();
+    await seedingServicesConversations();
   } catch (error) {
     console.error(error);
   } finally {
