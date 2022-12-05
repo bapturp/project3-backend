c0777f1a74f6d5353c0518ebe5f1c69e6bde1593
Baptiste Dromer <baptiste.dromer@gmail.com>
2022-12-05 19:14:25 +0100

    seeding done

--- seeds/seed.js
+++ seeds/seed.js
@@ -9,19 +9,20 @@ const Reservation = require('../models/Reservation.model');
 const Service = require('../models/Service.model');
 const ServiceItem = require('../models/ServiceItem.model');
 const Tag = require('../models/Tag.model');
+const Message = require('../models/Message.models');
+const Conversation = require('../models/Conversation.models');
 
 // seeds
 const tagSeed = require('./tag.seeds.json');
 const userSeed = require('./user.seeds.json');
 const servicesSeed = require('./services.seed.json');
+const reservationsSeed = require('./reservations.seed.json');
+const conversationsSeed = require('./conversations.seed.json');
 
 const seedingtags = async () => {
   try {
-    const deletedTags = await Tag.deleteMany();
-    console.log(`Deleted tags: ${deletedTags.deletedCount}`);
-
-    const createdTags = await Tag.create(tagSeed);
-    console.log(`Created tags: ${createdTags.length}`);
+    await Tag.deleteMany();
+    await Tag.create(tagSeed);
   } catch (error) {
     console.error(error);
   }
@@ -29,35 +30,73 @@ const seedingtags = async () => {
 
 const seedingUsers = async () => {
   try {
-    const deletedUsers = await User.deleteMany();
-    console.log(`Deleted users: ${deletedUsers.deletedCount}`);
-
-    const createdUsers = await User.create(userSeed);
-    console.log(`Created users: ${createdUsers.length}`);
+    await User.deleteMany();
+    await User.create(userSeed);
   } catch (error) {
     console.error(error);
   }
 };
 
-const seedingServicesConversations = async () => {
+const seedingServices = async () => {
   try {
-    deletedService = await Service.deleteMany();
+    await ServiceItem.deleteMany();
+    await Service.deleteMany();
+    await Tag.deleteMany();
+
     for (let service of servicesSeed) {
-      const { _id: providerId } = (await User.find({ username: service.provider }, { _id: 1 }))[0];
-      const consumerId = await User.find({ username: service.consumer })[0];
+      const { _id: providerId } = await User.findOne({ username: service.provider }, { _id: 1 });
 
       const tags = await Tag.find({ $in: service.tags });
 
-      const createdService = await Service.create({
+      const { _id: createdService } = await Service.create({
         title: service.title,
         description: service.description,
         tags,
         location: service.location,
         provider: providerId,
       });
-      console.log(createdService);
+
       for (let { startDate, endDate, price } of service.serviceItems) {
-        await ServiceItem.create({ startDate, endDate, price });
+        await ServiceItem.create({ startDate, endDate, price, service: createdService });
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const seedingReservations = async () => {
+  try {
+    await Reservation.deleteMany();
+
+    for (let { serviceItems, consumer: consumerName, provider: providerName } of reservationsSeed) {
+      const { _id: serviceItemId } = await ServiceItem.findOne({ startDate: serviceItems[0].startDate });
+      const consumer = await User.findOne({ username: consumerName });
+      const provider = await User.findOne({ username: providerName });
+      await Reservation.create({ consumer, provider, serviceItem: [serviceItemId] });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const seedingConversations = async () => {
+  try {
+    await Message.deleteMany();
+    await Conversation.deleteMany();
+
+    for (let { participants, service, messages } of conversationsSeed) {
+      const { _id: serviceId } = await Service.findOne({ title: service }, { _id: 1 });
+
+      const participantsId = (
+        await User.find({ $or: [{ username: participants[0] }, { username: participants[1] }] }, { _id: 1 })
+      ).map((e) => e._id);
+
+      const { _id: conversationId } = await Conversation.create({ participants: participantsId, service: serviceId });
+
+      for (let { sender, content } of messages) {
+        const { _id: senderId } = await User.findOne({ username: sender }, { _id: 1 });
+        await Message.create({ sender: senderId, content, conversation: conversationId });
       }
     }
   } catch (error) {
@@ -69,7 +108,9 @@ const seeding = async () => {
   try {
     await seedingUsers();
     await seedingtags();
-    await seedingServicesConversations();
+    await seedingServices();
+    await seedingReservations();
+    await seedingConversations();
   } catch (error) {
     console.error(error);
   } finally {
